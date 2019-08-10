dc6cf41f736772b1bb1d6eb0964fc70ff2d42a7b
yutaro <yutaro@yutaro.local>
2019-08-10 22:32:57 +0900

    edit#1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'sticky#index'
   get 'sticky' => 'sticky#index'
-  get 'sticky/new' => 'sticky#new'
+  get 'users/sticky/new' => 'sticky#new'
   post 'sticky' => 'sticky#create'
   get 'users/:id' => 'users#show'
 end
